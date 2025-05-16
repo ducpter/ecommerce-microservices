253cfa1ec2ec238f8e5fb3b94de0afffba12fc4f
huynguyen1908 <144637476+huynguyen1908@users.noreply.github.com>
2025-05-16 16:49:57 +0700

    change keycloak deployment

---
+++ api-gateway/src/main/java/com/ibatulanand/apigateway/config/CorsGlobalConfig.java
@@ -0,0 +1,24 @@
+package com.ibatulanand.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsGlobalConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:4200");
+        config.setAllowCredentials(true); // Nếu có token Cookie
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
